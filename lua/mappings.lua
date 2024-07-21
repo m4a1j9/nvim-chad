8147bda134b72e2f7930981ded684fc0835e9e4b
m4a1j9 <davidovandre5@gmail.com>
2024-07-21 18:42:07 +0700

    rm neoclip

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,7 +6,6 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jj", "<ESC>")
-map("n", "<leader>y", "<cmd>Telescope neoclip<CR>", { desc = "Open Yank History" })
 map(
   "n",
   "<leader>fg",
